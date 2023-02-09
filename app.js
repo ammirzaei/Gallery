579cc1538197433f7031aeb4655e9dec0b3300ac
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-02-09 21:30:27 +0330

    use environment variables with dotenv and cross-env module

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const dotenv = require('dotenv');
 
 const app = new express();
 
 const { setRoutes } = require('./utils/routes');
 const { setStatics } = require('./utils/statics');
+
+// config process.env
+dotenv.config({ path: './config/config.env' })
 
 // use static
 setStatics(app);
@@ -11,6 +15,6 @@ setStatics(app);
 // use routes
 setRoutes(app);
 
-app.listen(3000, () => {
-    console.log('Running app on port 3000');
+app.listen(process.env.PORT, () => {
+    console.log(`Running app on port '${process.env.PORT}' in mode '${process.env.NODE_ENV}'`);
 });
