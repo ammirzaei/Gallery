bf50267a1329dd82145a37082be304688a52b09d
AmirMohammad <89526419+ammirzaei@users.noreply.github.com>
2023-02-13 21:36:15 +0330

    change dashboard directory

--- middlewares/routes.js
+++ middlewares/routes.js
@@ -1,7 +1,7 @@
 const { authenticated } = require('./auth');
 
 module.exports.setRoutes = (app) => {
-    app.use('/dashboard', authenticated, require('./../routes/dashboardRoute'))
+    app.use('/dashboard', authenticated, require('./../routes/dashboard/dashboardRoute'))
     app.use(require('./../routes/userRoute'))
     app.use(require('./../routes/homeRoute'));
 }
--- routes/dashboard/dashboardRoute.js
+++ routes/dashboard/dashboardRoute.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
-const dashboardController = require('./../controllers/dashboardController');
+const dashboardController = require('./../../controllers/dashboard/dashboardController');
 const router = new Router();
 
 // Home Page -- GET
